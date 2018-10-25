b2b0299aba1f88fcd5e37f94d66f933d858a3b51
Ivan <proekt.energetika@gmail.com>
2018-10-25 10:17:22 +0300

    Разметил, стилизовал и адаптировал блок clients

--- source/js/slider-init.js
+++ source/js/slider-init.js
@@ -8,4 +8,45 @@
       speed: 500
     });
   });
+
+  $(document).ready(function(){
+    $('.team__list').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
+
+  $(document).ready(function(){
+    $('.clients__list').slick({
+      infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 6,
+      responsive: [
+        {
+          breakpoint: 1139,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }
+      ]
+    });
+  });
 })();
