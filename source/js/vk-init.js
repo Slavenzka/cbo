38b0cfc3699cadde32b3635f9709eb422bdd0427
Ivan <proekt.energetika@gmail.com>
2018-11-21 23:26:39 +0300

    Оптимизация v.2

--- source/js/vk-init.js
+++ source/js/vk-init.js
@@ -1,5 +1,7 @@
 'use strict';
 
 // (function () {
+if (window.matchMedia('screen and (min-width: 720px)').matches) {
   VK.Widgets.Group("vk_groups", {mode: 3, no_cover: 1, color3: '005856', width: 328}, 45750738);
+}
 // })();
