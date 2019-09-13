bc30cd7ec51c5153f11c0fa6654ae675035b8bf6
Ivan <i.romanov@protonmail.com>
2019-09-13 22:01:58 +0300

    Вынес логотипы из отзывов в ленивую загрузку

--- source/js/vk-init.js
+++ source/js/vk-init.js
@@ -1,19 +1,7 @@
 'use strict';
 
-// (function () {
 if (window.matchMedia('screen and (min-width: 720px)').matches) {
-  // VK.Widgets.Group("vk_groups", {mode: 3, no_cover: 1, color3: '005856', width: 328}, 45750738);
-  //
   window.vkAsyncInit = function() {
-    VK.Widgets.Group("vk_groups", {mode: 3, no_cover: 1, color3: '005856', width: 328}, 45750738); //указывается ID группы лишь потому что это пример
+    VK.Widgets.Group("vk_groups", {mode: 3, no_cover: 1, color3: '005856', width: 328}, 45750738);
   };
-
-  // setTimeout(function() {
-  //   var el = document.createElement("script");
-  //   el.type = "text/javascript";
-  //   el.src = "https://vk.com/js/api/openapi.js?156";
-  //   el.async = true;
-  //   document.getElementById("vk_groups").appendChild(el);
-  // }, 0);
 }
-// })();
